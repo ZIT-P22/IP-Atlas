9c0348cfdfab551c48cc8d5f8030b420330eaafa
FischLord <janneck3@web.de>
2024-01-29 14:39:16 +0100

    ipv4 filter works about 50%

--- ip-atlas/filter.py
+++ ip-atlas/filter.py
@@ -5,28 +5,22 @@ from helper import *
 # this function filter the json document for any ip address which maches the pattern
 # example filter could be *.168.*.2 (* menas it can be any number)
 def filterByIp(octed1, octed2, octed3, octed4, data):
-    if not checkJsonEmpty():
-        for x in data["hosts"]:
-            if not checkIfInputIsNone(octed1):
-                data = [x for x in data["hosts"] if devideIp(x["ip"])[0] == int(octed1)]
-            if not checkIfInputIsNone(octed2):
-                data = [x for x in data if devideIp(x["ip"])[1] == octed2]
-            if not checkIfInputIsNone(octed3):
-                data = [x for x in data if devideIp(x["ip"])[2] == octed3]
-            if not checkIfInputIsNone(octed4):
-                data = [x for x in data if devideIp(x["ip"])[3] == octed4]
-        return data
-    else:
-        print("Json document is empty")
+    # convert all octeds to int
+    result = []
+    for i in range(len(data["hosts"])):
+        ip = devideIp(data["hosts"][i]["ip"])
+        # print(ip)
+        # print("Oktet 1:",octed1,"Oktet 2:",octed2,"Oktet 3:",octed3,"Oktet 4:",octed4)
+        if ip[0] == octed1 or octed1 == "*":
+            if ip[1] == octed2 or octed2 == "*":
+                if ip[2] == octed3 or octed3 == "*":
+                    if ip[3] == octed4 or octed4 == "*":
+                        result.append(data["hosts"][i])
+                        print(data["hosts"][i]["ip"])
+
 
 # filter by name/name section
-def filterByName(name, data):
-    if not checkJsonEmpty():
-        if not checkIfInputIsNone(name):
-            data = [x for x in data["hosts"] if name in x["name"]]
-        return data
-    else:
-        print("Json document is empty")
+
 # filter by ports
 
 # filter by tags
@@ -39,4 +33,4 @@ def filterByName(name, data):
 
 
 #! tests
-print(filterByIp("192", "168", "0", "", loadJson()))
+print(filterByIp("192", "*", "*", "*", loadJson()))
