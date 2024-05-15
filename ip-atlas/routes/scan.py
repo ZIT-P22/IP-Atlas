75160af99119a2720e8c9b0bbaa05e846ad2886a
FischLord <janneck3@web.de>
2024-05-15 19:39:56 +0200

    feat: Add check_ipv4 endpoint to scan route

--- ip-atlas/routes/scan.py
+++ ip-atlas/routes/scan.py
@@ -1,12 +1,13 @@
 from utils.scanner import scan_devices
 from flask import Blueprint, jsonify, render_template
 from utils.crud import convert_discovered_devices_to_json_format, get_tags, set_used_of_discovered_device
+from models import Host
 
 scan = Blueprint("scan", __name__)
 
 @scan.route("/discovered")
 def discovered():
-    scan_devices("192.168.42.162/24","wlo1")
+    scan_devices("172.23.87.15/20","eth0")
     devices = convert_discovered_devices_to_json_format()
     tags = get_tags()
     print(devices)
@@ -18,3 +19,10 @@ def set_used(id):
     status = set_used_of_discovered_device(id)
     return jsonify({"status": status})
 
+@scan.route("/check_ipv4/<ipv4>", methods=["GET"])
+def check_ipv4(ipv4):
+    device = Host.query.filter_by(ipv4=ipv4).first()
+    if device:
+        return jsonify({"exists": True})
+    else:
+        return jsonify({"exists": False})
